69b39ce9b6e1363fc0bb1e1f017f1db73550b327
Rahul <mailtorahul724@gmail.com>
2018-06-29 17:25:19 +0530

    properly compiled

--- MycollectionTest/ArrayHelper.cs
+++ MycollectionTest/ArrayHelper.cs
@@ -5,55 +5,69 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MycollectionTest
-
 {
     public static class ArrayHelper
     {
         static void Main()
         {
-           
+            //Defining Size
             Console.WriteLine("Enter no of records");
-            int size = Convert.ToInt16( Console.ReadLine());
-            Person[] array = new Person[size];
-            for (int i=0;i<size;i++)
-            {
-                
+            int size = Convert.ToInt16(Console.ReadLine());
 
+            Person[] array = new Person[size];
+
+            //Input Data
+            for (int i = 0; i < size; i++)
+            {
                 if (array[i] == null)
                 {
-                    Person[] values = new Person[i];
-                    Console.WriteLine("Enter User id:  ");
+                    Console.Write("Enter User id:");
                     var s = Convert.ToString(Console.ReadLine());
-                   // array1[i].UserId = s;
-                    array[i].UserId=s.ToString();
-                    Console.WriteLine("Enter Name:  ");
+                    Console.Write("Enter Name:");
                     string n = Console.ReadLine();
-                    array[i].Pname = n;
-                    Console.WriteLine("Enter Age:  ");
+                    Console.Write("Enter Age:");
                     int A = Convert.ToInt16(Console.ReadLine());
-                    array[i].Age =A;
-                    Console.WriteLine("Enter rollNo:  ");
-                    A = Convert.ToInt16(Console.ReadLine());
-                    array[i].RollNo = A;
+                    Console.Write("Enter rollNo:");
+                    int R = Convert.ToInt16(Console.ReadLine());
 
-                    int count = 0;
-                    count++;
-                    
+                    InputDetails(s, n, A, R, array, i);
                 }
-                else
-                {
-                    Console.WriteLine("Array is already full");
-                    Console.ReadLine();
 
-                }
-               
+                 xprintDetails(array);
             }
 
-
-           
+            Console.WriteLine("Array is already full");
+            Console.ReadLine();
         }
 
-        
+        private static void xprintDetails(Person[] array)
+        {
+            var list = array.ToList();
 
+            foreach (var value in list)
+            {
+                Console.WriteLine(value.UserId);
+                Console.WriteLine(value.Pname);
+                Console.WriteLine(value.Age);
+                Console.WriteLine(value.RollNo);
+            }
+
+            
+        }
+
+        private static void InputDetails(string s, string n, int A, int R, Person[] array, int i)
+        {
+            Person Pname = new Person();
+
+            Pname.UserId = s;
+            Pname.Pname = n;
+            Pname.Age = A;
+            Pname.RollNo = R;
+
+            //Store value in array 
+            array[i] = Pname;
+        }
+
+       
     }
 }
--- MycollectionTest/Person.cs
+++ MycollectionTest/Person.cs
@@ -8,37 +8,34 @@ namespace MycollectionTest
 {
     public class Person
     {
-        //public Person(string UserId, string Pname, int RollNo, int Age)
-        //{
-        //}
-
         public string UserId { get; set; }
         public string Pname { get; set; }
         public int Age { get; set; }
         public int RollNo { get; set; }
-     
-        //use when you have to pass value to other vaiable of other function.
-        /*
-    {
-        get { return UserId; }
-        set { UserId = value; }
     }
+   
+    //use when you have to pass value to other vaiable of other function.
+    /*
+{
+    get { return UserId; }
+    set { UserId = value; }
+}
 
-    public string PName
-    {
-        get { return PName; }
-        set { PName = value; }
-    }
-    public int RollNo        {
-        get { return RollNo; }
-        set { RollNo = value; }
-    }
-    public int Age
-    {
-        get { return Age; }
-        set { Age = value; }
-    }
-    */
-    }
+public string PName
+{
+    get { return PName; }
+    set { PName = value; }
+}
+public int RollNo        {
+    get { return RollNo; }
+    set { RollNo = value; }
+}
+public int Age
+{
+    get { return Age; }
+    set { Age = value; }
+}
+*/
+
 }
 
